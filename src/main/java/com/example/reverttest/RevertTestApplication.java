ea4a21887f88a5f1c2f3c74533815c52c5e0c733
이승휘 <seunghwi.lee@navercorp.com>
2023-05-10 19:09:53 +0900

    init

---
+++ src/main/java/com/example/reverttest/RevertTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.reverttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevertTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevertTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reverttest/RevertTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reverttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevertTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
